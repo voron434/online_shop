cfdfed42dfdcef469c0571326fec434fc8181cb7
Ilya Osipov <070809010@mail.ru>
2017-08-22 20:50:34 +0500

    Fix typo

--- marketplace/views.py
+++ marketplace/views.py
@@ -13,35 +13,33 @@ def clothes_draft_list(request):
     return render(request, 'marketplace/clothes_list.html', {'Clothes': Clothes})
 
 
-@login_required
 def clothes_new(request):
-        if request.method == "POST":
-            form = ClothesForm(request.POST)
-            if form.is_valid():
-                clothes = form.save(commit=False)
-                clothes.author = request.user
-                clothes.save()
-                return redirect('clothes_detail', pk=clothes.pk)
-        else:
-            form = clothesForm()
-        return render(request, 'marketplace/clothes_edit.html', {'form': form})
+    if request.method == "POST":
+        form = ClothesForm(request.POST)
+        if form.is_valid():
+            clothes = form.save(commit=False)
+            clothes.author = request.user
+            clothes.save()
+            return redirect('clothes_detail', pk=clothes.pk)
+    else:
+        form = ClothesForm()
+    return render(request, 'marketplace/clothes_edit.html', {'form': form})
 
 
-@login_required
 def clothes_edit(request, pk):
-		clothes = get_object_or_404(Post, pk=pk)
-		if request.method == "POST":
-		    form = ClothesForm(request.POST, instance=clothes)
-		    if form.is_valid():
-		        clothes = form.save(commit=False)
-		        clothes.author = request.user
-		        clothes.save()
-		        return redirect('clothes_detail', pk=clothes.pk)
-		else:
-		    form = clothesForm(instance=clothes)
-		return render(request, 'marketplace/clothes_edit.html', {'form': form})
+	clothes = get_object_or_404(Clothes, pk=pk)
+	if request.method == "POST":
+	    form = ClothesForm(request.POST, instance=clothes)
+	    if form.is_valid():
+	        clothes = form.save(commit=False)
+	        clothes.author = request.user
+	        clothes.save()
+	        return redirect('clothes_detail', pk=clothes.pk)
+	else:
+	    form = ClothesForm(instance=clothes)
+	return render(request, 'marketplace/clothes_edit.html', {'form': form})
 
 
 def clothes_detail(request, pk):
-        clothes = get_object_or_404(clothes, pk=pk)
-        return render(request, 'marketplace/clothes_detail.html', {'clothes': clothes})
+    clothes = get_object_or_404(Clothes, pk=pk)
+    return render(request, 'marketplace/clothes_detail.html', {'clothes': clothes})
