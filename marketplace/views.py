0a389f55d2c436ff128cf98dc46a6720aa3b0c01
Ilya Osipov <070809010@mail.ru>
2017-08-22 18:59:17 +0500

    Add view for product lists

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from .forms import ProductForm
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.filter(is_published=True).order_by('type')
+    return render(request, 'shop/product_list.html', {'products': products})
+
+
+def product_draft_list(request):
+    products = Product.objects.filter(is_published=False).order_by('type')
+    return render(request, 'shop/product_draft_list.html', {'products': products})
