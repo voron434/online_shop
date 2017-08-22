07067ae8272ea9a1acc7de62a32ae74881cbd49a
Ilya Osipov <070809010@mail.ru>
2017-08-22 20:26:39 +0500

    Add ability to create and edit clothes cards

--- marketplace/views.py
+++ marketplace/views.py
@@ -11,3 +11,37 @@ def clothes_list(request):
 def clothes_draft_list(request):
     clothes = Clothes.objects.filter(is_published=False).order_by('type')
     return render(request, 'marketplace/clothes_list.html', {'Clothes': Clothes})
+
+
+@login_required
+def clothes_new(request):
+        if request.method == "POST":
+            form = ClothesForm(request.POST)
+            if form.is_valid():
+                clothes = form.save(commit=False)
+                clothes.author = request.user
+                clothes.save()
+                return redirect('clothes_detail', pk=clothes.pk)
+        else:
+            form = clothesForm()
+        return render(request, 'marketplace/clothes_edit.html', {'form': form})
+
+
+@login_required
+def clothes_edit(request, pk):
+		clothes = get_object_or_404(Post, pk=pk)
+		if request.method == "POST":
+		    form = ClothesForm(request.POST, instance=clothes)
+		    if form.is_valid():
+		        clothes = form.save(commit=False)
+		        clothes.author = request.user
+		        clothes.save()
+		        return redirect('clothes_detail', pk=clothes.pk)
+		else:
+		    form = clothesForm(instance=clothes)
+		return render(request, 'marketplace/clothes_edit.html', {'form': form})
+
+
+def clothes_detail(request, pk):
+        clothes = get_object_or_404(clothes, pk=pk)
+        return render(request, 'marketplace/clothes_detail.html', {'clothes': clothes})
