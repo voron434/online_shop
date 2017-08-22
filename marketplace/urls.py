2ea6f87a1784f2a60fd855c1682e3e5ba71d2a6c
Ilya Osipov <070809010@mail.ru>
2017-08-22 20:47:13 +0500

    Add new urls for new functionality

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.clothes_list, name='clothes_list'),
+    url(r'^clothes/new/$', views.clothes_new, name='clothes_new'),
+    url(r'^clothes/(?P<pk>[0-9]+)/$', views.clothes_detail, name='clothes_detail'),
+    url(r'^clothes/(?P<pk>[0-9]+)/edit/$', views.clothes_edit, name='clothes_edit'),
 ]
