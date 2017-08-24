96c44b1be5da4fc02f85a64d495f76acaef6c294
Ilya Osipov <070809010@mail.ru>
2017-08-24 14:12:14 +0500

    Add drafts url

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.clothes_list, name='clothes_list'),
+    url(r'^drafts/$', views.clothes_draft_list, name='clothes_draft_list'),
     url(r'^clothes/new/$', views.clothes_new, name='clothes_new'),
     url(r'^clothes/(?P<pk>[0-9]+)/$', views.clothes_detail, name='clothes_detail'),
     url(r'^clothes/(?P<pk>[0-9]+)/edit/$', views.clothes_edit, name='clothes_edit'),
