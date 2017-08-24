cc1c51d6b91f21079c26f8560fd5444a7b2e0632
Ilya Osipov <070809010@mail.ru>
2017-08-24 19:27:54 +0500

    Add publish method and gender filters

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     url(r'^clothes/new/$', views.clothes_new, name='clothes_new'),
     url(r'^clothes/(?P<pk>[0-9]+)/$', views.clothes_detail, name='clothes_detail'),
     url(r'^clothes/(?P<pk>[0-9]+)/edit/$', views.clothes_edit, name='clothes_edit'),
+    url(r'^clothes/(?P<pk>\d+)/publish/$', views.clothes_publish, name='clothes_publish'),
+    url(r'^clothes/filter/mens$', views.filter_clothes_mens, name='filter_clothes_mens'),
+    url(r'^clothes/filter/womens$', views.filter_clothes_womens, name='filter_clothes_womens'),
+    url(r'^clothes/filter/child$', views.filter_clothes_child, name='filter_clothes_child'),
+
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -43,3 +43,28 @@ def clothes_edit(request, pk):
 def clothes_detail(request, pk):
     clothes = get_object_or_404(Clothes, pk=pk)
     return render(request, 'marketplace/clothes_detail.html', {'clothes': clothes})
+
+
+def clothes_publish(request, pk):
+    clothes = get_object_or_404(Clothes, pk=pk)
+    clothes.publish()
+    return clothes_draft_list(request)
+
+
+def clothes_delete(request):
+    pass
+
+
+def filter_clothes_mens(request):
+    clothes = Clothes.objects.filter(for_whom="M").order_by('type')
+    return render(request, 'marketplace/clothes_list.html', {'clothes': clothes})
+
+
+def filter_clothes_womens(request):
+    clothes = Clothes.objects.filter(for_whom="F").order_by('type')
+    return render(request, 'marketplace/clothes_list.html', {'clothes': clothes})
+
+
+def filter_clothes_child(request):
+    clothes = Clothes.objects.filter(for_whom="C").order_by('type')
+    return render(request, 'marketplace/clothes_list.html', {'clothes': clothes})
